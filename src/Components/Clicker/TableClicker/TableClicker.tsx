5463aa46eb9089d898ebda6424ecb59872bf987e
Astaroth <minatosavich@gmail.com>
2021-07-27 15:18:14 +0300

    feat: add red color currentValue className

--- src/Components/Clicker/TableClicker/TableClicker.tsx
+++ src/Components/Clicker/TableClicker/TableClicker.tsx
@@ -17,14 +17,14 @@ type TableClickerType = {
 export const TableClicker: React.FC<TableClickerType> = React.memo(
     ({error, currentValue, onChange, changeMode, maxValue, minValue}) => {
 
-        const pressSet = "enter values and press 'set'"
-        const incorrectValue = "incorrect value"
+        const pressSetText = "enter values and press 'set'"
+        const incorrectValueText = "incorrect value"
         const increment = 1;
 
         const resultIncrementValue = increment + currentValue
         const disabledIncrementButton = changeMode || maxValue === currentValue
         const disabledResetButton = changeMode || minValue === currentValue
-        const currentText = !changeMode ? currentValue : error ? incorrectValue : pressSet
+        const currentText = !changeMode ? currentValue : error ? incorrectValueText : pressSetText
         const textStyle = !changeMode ? s.text : error ? s.error : s.set
 
         const onClickIncrement = () => maxValue >= resultIncrementValue
@@ -35,7 +35,7 @@ export const TableClicker: React.FC<TableClickerType> = React.memo(
         return (
             <div className={s.container}>
                 <div className={s.wrapperText}>
-                    <p className={textStyle}>{currentText}</p>
+                    <p className={`${maxValue === currentValue && !changeMode ? s.red + " " + s.text : textStyle}`}>{currentText}</p>
                 </div>
                 <div className={s.wrapperBtn}>
                     <Button variant={"contained"}
